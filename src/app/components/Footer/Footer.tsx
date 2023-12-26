8bf9201659984a25fce33eb7a68cbea56d07ed6d
Mshujon <mshujon@MKs-MacBook-Pro.local>
2023-12-26 13:47:39 +0100

    Homepage done

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import MaxWithWrap from "../MaxWithWrapper/MaxWithWrap";
+
+const Footer = () => {
+    return (
+        <div>
+            <div className=" bg-gray-50  pb-10">
+            <hr />
+            <MaxWithWrap>
+              <div className="grid grid-cols-4 gap-5 my-20 relative">
+                  <div>
+                    <h2 className="text-lg font-semibold pb-2">Product</h2>
+                     <ul className="leading-8">
+                        <li className="hover:text-main"><Link href="">LinkedIn Post Ideas</Link></li>
+                        <li className="hover:text-main"><Link href="">LinkedIn Post Generator</Link></li>
+                        <li className="hover:text-main"><Link href="">LinkedIn Carousel Maker</Link></li>
+                        <li className="hover:text-main"><Link href="">LinkedIn Post Scheduling</Link></li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h2 className="text-lg font-semibold pb-2">Company</h2>
+                     <ul className="leading-8">
+                        <li className="hover:text-main"><Link href="">Team</Link></li>
+                        <li className="hover:text-main"><Link href="">Roadmap</Link></li>
+                        <li className="hover:text-main"><Link href="">Privacy Policy</Link></li>
+                        <li className="hover:text-main"><Link href="">Trams of Service</Link></li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h2 className="text-lg font-semibold pb-2">Resources</h2>
+                     <ul className="leading-8">
+                        <li className="hover:text-main"><Link href="">For Agency</Link></li>
+                        <li className="hover:text-main"><Link href="">Our Wall Of Love</Link></li>
+                        <li className="hover:text-main"><Link href="">ChatGPT vs Superpost</Link></li>
+                        <li className="hover:text-main"><Link href="">Post Preview & Styling</Link></li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h2 className="text-lg font-semibold pb-2">Keep in touch</h2>
+                     <ul className="leading-8">
+                        <li className="hover:text-main"><Link href="">Whatsapp</Link></li>
+                        <li className="hover:text-main"><Link href="">LinkedIn</Link></li>
+                        <li className="hover:text-main"><Link href="">Twitter</Link></li>
+                        <li className="hover:text-main"><Link href="">Email</Link></li>
+                    </ul>
+                  </div>
+              </div>
+              <hr />
+              <div className="py-5 text-center">
+                    <p className="font-semibold text-sm">Copyright © 2023 Superpost. All rights reserved.</p>
+                    <p className="py-2 text-gray-700 text-sm">Disclaimer: Supergrow is not endorsed by or affiliated with the LinkedIn Corporation, registered in the U.S. and other countries. LinkedIn is a trademark of the LinkedIn Corporation.</p>
+              </div>
+            </MaxWithWrap>   
+    </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,34 @@ import productImg from "../../public/product.webp"
 import styles from "./body.module.css"
 import MaxWithWrap from "./components/MaxWithWrapper/MaxWithWrap"
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { IoMdCheckmark } from "react-icons/io";
 export default function Home() {
   
   return (
    <div>
+    {/* Hero section started */}
     <MaxWithWrap>
       <div className={`${styles.bacroungPattern} text-center`}>
         <div className="flex flex-col justify-center items-center pt-20 p-5">
           <h1 className="text-6xl font-bold w-4/5 text-center p-5">The only LinkedIn content creation tool you’ll ever need</h1>
           <p className="text-xl w-3/6 text-center text-gray-600 py-5">Generate content ideas, high-quality LinkedIn posts, make eye-catching carousels, format for readability, and schedule easily—all in one place!</p> 
-          <button className="p-4 mt-5 bg-[#5371ff] text-white font-semibold rounded-full inline-flex">Get started for free <span className="inline-flex text-2xl ms-2 px-px bg-gray-50 rounded-full text-[#5371ff]"><HiArrowNarrowRight/></span></button>
+          <button className="p-4 mt-5 bg-main text-white font-semibold rounded-full inline-flex">Get started for free <span className="inline-flex text-2xl ms-2 px-px bg-gray-50 rounded-full text-main"><HiArrowNarrowRight/></span></button>
         </div>
       </div>
       
     </MaxWithWrap>
+    {/* Reviews section started */}
     <div className={`${styles.review}  h-20 `}>
-          <div className={`${styles.dashboard} flex justify-center`}>
+          <div className={`${styles.dashboard} flex justify-center mb-10`}>
               <Image className="w-5/6 shadow-md" src={productImg} alt="product demo"></Image>
           </div>
     </div>
-    <div className="text-center  bg-[#f5f4f4] mx-8 py-10">
+    <div className="text-center  bg-gray-50 mx-8 py-10">
             <h2 className="mt-10 text-4xl font-bold ">Loved by Professionals</h2>
             <MaxWithWrap>
               <div className="grid grid-cols-3 gap-12 mt-20 text-xl relative">
                   <div className="bg-white p-8 shadow-md rounded-xl text-left">
-                    <p>I love Supergrow. The carousel feature is awesome. Life is so much easier! The customer service is outstanding. So personable. Thanks, guys.</p>
+                    <p>I love Superpost. The carousel feature is awesome. Life is so much easier! The customer service is outstanding. So personable. Thanks, guys.</p>
                     <div className="mt-10 pt-5 flex justify-start absolute bottom-8">
                         <img src="/user/ant.jpg" className="w-12 h-12 rounded-full" alt="user" />
                         <div className="ps-4">
@@ -37,7 +40,7 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="bg-white p-8 shadow-md rounded-xl text-left">
-                    <p>I love Supergrow, it is by far my favorite LinkedIn app! The templates are super helpful, and the AI works great. With Supergrow, I save at least 4 hours per week.</p>
+                    <p>I love Superpost, it is by far my favorite LinkedIn app! The templates are super helpful, and the AI works great. With Supergrow, I save at least 4 hours per week.</p>
                     <div className="mt-10  pt-5 flex justify-start ">
                         <img src="/user/emonji.png" className="w-12 h-12 rounded-full" alt="user" />
                         <div className="ps-4">
@@ -47,7 +50,7 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="bg-white p-8 shadow-md rounded-xl text-left">
-                    <p>My experience with Supergrow has been amazing, I love the app and it is super polished. If you do Linkined business then it's a Green light!</p>
+                    <p>My experience with Superpost has been amazing, I love the app and it is super polished. If you do Linkined business then it's a Green light!</p>
                     <div className="mt-10 pt-5 flex justify-start absolute bottom-8">
                         <img src="/user/projapoti.jpg" className="w-12 h-12 rounded-full" alt="user" />
                         <div className="ps-4">
@@ -57,9 +60,25 @@ export default function Home() {
                     </div>
                   </div>
               </div>
+              
             </MaxWithWrap>   
     </div>
-
+    {/* post section */}
+    <section className="bg-gray-50">
+        <MaxWithWrap>
+        <div className="post my-20 text-left">
+                        <h1 className="text-5xl font-bold py-8  ">LinkedIn posts on autopilot</h1>
+                        <p className="text-xl leading-8">Experience the convenience of automated post-generation. Share your topic and role, and receive expertly tailored posts every week. Effortless content creation made possible!</p>
+                        <ul className="text-xl py-5 leading-10">
+                          <li><span className="inline-block text-main"><IoMdCheckmark/></span> Share your details</li>
+                          <li><span className="inline-block text-main"><IoMdCheckmark/></span> Receive fresh and high-quality posts without lifting a finger.</li>
+                        </ul>
+                        <button className="p-4 mt-5 mb-10 hover:bg-main hover:text-white text-main border border-main font-semibold rounded-full inline-flex">Get started for free </button>
+                  </div>
+        </MaxWithWrap>
+    </section>
+    {/* keyword sliding section started */}
+    <h1 className="text-center text-4xl py-5 mt-32 font-bold">Everything else you need</h1>
     <div className="slide mt-10  overflow-x-hidden">
       <div className={`${styles.sliding} p-0 whitespace-nowrap `}>
           <ul className=" inline-block whitespace-nowrap  text-xl font-semibold ">
@@ -116,6 +135,22 @@ export default function Home() {
           </ul>
       </div>
     </div>
+    <section className="mt-28">
+      <div className={`${styles.grow}  mt-32 `}>
+      <div className={` text-center`}>
+        <div className="flex flex-col justify-center items-center pt-20 p-5">
+          <h1 className="text-6xl font-bold w-full text-center p-5">Grow on LinkedIn with Supergrow</h1>
+          <p className="text-xl w-5/6 text-center text-gray-600 py-5">row your personal brand on LinkedIn the simple, powerful, and affordable way with Supergrow. Hundreds of professionals and agencies trust Supergrow to save them time and help them grow their audience.</p> 
+          <button className="p-4 mt-5 bg-main text-white font-semibold rounded-full inline-flex">Get started for free <span className="inline-flex text-2xl ms-2 px-px bg-gray-50 rounded-full text-main"><HiArrowNarrowRight/></span></button> 
+          <ul className="text-sm pt-5 pb-16 leading-10 flex justify-center">
+             <li><span className="inline-block text-main"><IoMdCheckmark/></span> No credit card required</li>
+            <li><span className="inline-block ms-5 text-main"><IoMdCheckmark/></span> 7-day free trial</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+      
+    </section>
   </div>
   )
 }
